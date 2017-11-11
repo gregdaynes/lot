f4076839fbd9f5ae2e87db7c6f0330989a3c4d6b
Gregory Daynes <greg@stembolt.com>
2017-11-10 20:30:10 -0800

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
+  config.after(:each) do
+    Feature.class_variable_set :@@feature, Set.new
+  end
+end
